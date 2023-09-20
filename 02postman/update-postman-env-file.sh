63b500a8277dea6f207647790d9c1f579c9f9ff6
scali <124896477+scali123@users.noreply.github.com>
2023-09-20 17:43:35 +0200

    Update update-postman-env-file.sh

--- 02postman/update-postman-env-file.sh
+++ 02postman/update-postman-env-file.sh
@@ -9,9 +9,9 @@ api_gateway_url=`aws cloudformation describe-stacks \
   --stack-name petstore-api-stack \
   --query "Stacks[0].Outputs[*].{OutputValueValue:OutputValue}" --output text`
 
-echo "API Gateway URL:" ${api_gateway_url}
+echo "API Gateway URL: https://skntgzvovc.execute-api.eu-west-1.amazonaws.com/prod" 
 
-jq -e --arg apigwurl "$api_gateway_url" '(.values[] | select(.key=="apigw-root") | .value) = $apigwurl' \
+jq -e --arg apigwurl https://skntgzvovc-vpce-0b27ccb25401f01a2.execute-api.eu-west-1.amazonaws.com/prod '(.values[] | select(.key=="apigw-root") | .value) = $apigwurl' \
   PetStoreAPIEnvironment.postman_environment.json > PetStoreAPIEnvironment.postman_environment.json.tmp \
   && cp PetStoreAPIEnvironment.postman_environment.json.tmp PetStoreAPIEnvironment.postman_environment.json \
   && rm PetStoreAPIEnvironment.postman_environment.json.tmp
